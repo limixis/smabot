c7e4418c30923e58f9e863d9eff6b230c4ce9a79
limixis <limixis@gmail.com>
2016-11-16 13:32:05 +0300

    Fixed issues, translated into Russian, stats pretty

--- sma_bot.py
+++ sma_bot.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardHide
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from validator import Validator
@@ -9,11 +9,11 @@ from collections import Counter
 import argparse
 
 
-TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-KEYS_FILE = 'C:\Users\limixis\Documents\GitHub\smabot\keys.tsv'
-LOG_FILE = 'C:\Users\limixis\Documents\GitHub\smabot\log.tsv'
+TOKEN = ''
+KEYS_FILE = '.../keys.tsv'
+LOG_FILE = '.../log.tsv'
 
-BANDS = [u'Авиарежим', u'Полдень на латинском', u'The Last Realism', u'4 Quarters Of A Pizza', u'', u'Всё никак', u'One piece band', u'', 'Banda']
+BANDS = [u'Авиарежим', u'Полдень на латинском', u'The Last Realism',u'4 Quarters Of A Pizza', u'Всё никак',u'One piece band',u'Age of Despration',u'My Desolation']
 
 reply_keyboard = [BANDS[i:i + 2] for i in range(0, len(BANDS), 2)]
 confirm_keyboard = [[u'Да', u'Нет']]
@@ -21,14 +21,15 @@ confirm_keyboard = [[u'Да', u'Нет']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
 markup_confirm = ReplyKeyboardMarkup(confirm_keyboard, one_time_keyboard=True, resize_keyboard=True)
 
+
 band_stats = Counter()
 
-ADMIN_NAMES = ['limixis', 'oneunreadmail']
+ADMIN_NAMES = ['limixis', 'oneunreadmail', 'Snufkin1514']
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Generate tokens for authentication")
-    parser.add_argument("-f", dest="key_file", default="./keys.tsv", help="Path to file with keys")
+    parser.add_argument("-f", dest="key_file", default="./smabot-dev/keys.tsv", help="Path to file with keys")
     parser.add_argument("--bands", dest="bands", nargs="*", default=None, help="List of bands")
     parser.add_argument("--admins", dest="admins", nargs="*", default=['limixis', 'oneunreadmail'], help="List of admin usernames")
 
@@ -43,14 +44,17 @@ def start(bot, update):
 def stats(bot, update):
     telegram_user = update.message.from_user
     if telegram_user.username in ADMIN_NAMES:
+        data = ""
+        for band, rating in band_stats.most_common(len(band_stats)):
+            data += "{}: {}\n".format(band.encode('utf-8'), rating)
         bot.sendMessage(chat_id=update.message.chat_id,
-                        text=band_stats.items())
+                        text=data)
 
 
 class Chatter(object):
     def __init__(self):
         self.modes = {'AUTHENTICATION': self.process_token, 'VOTING': self.vote, 'FEEDBACK': self.send_feedback,
-                      self.confirm: 'CONFIRMATION'}
+                      'CONFIRMATION': self.confirm}       
         self.key_val = Validator(KEYS_FILE)
 
     def handle_text(self, bot, update, user_data):
@@ -76,30 +80,30 @@ class Chatter(object):
             message = u'Вы точно хотите проголосовать за ' + band + u'?'
             user_data['band'] = band
             user_data['mode'] = 'CONFIRMATION'
+            _markup = markup_confirm
         else:
             message = u'Ээ... эта группа сегодня не выступала. Выберите группу из списка.'
+            _markup = markup
         bot.sendMessage(chat_id=update.message.chat_id,
-                        text=message)
-
+                        text=message, reply_markup = _markup)
+    
     def confirm(self, bot, update, user_data):
-        _markup=None
-        answer = update.message.text.decode('utf-8')
-        if answer.lower() == u'да':
+        _markup = None
+        answer = update.message.text.lower().strip()
+        if answer == u'да':
             band = user_data['band']
             band_stats[band] += 1
             with open(LOG_FILE, 'a') as f:
                 print >> f, band.encode('utf-8')
             message = u'Отличный выбор, спасибо!'
             user_data['mode'] = 'AUTHENTICATION'
-        elif answer.lower() == u'нет':
+        elif answer == u'нет':
             message = u'Передумали? Тогда проголосуйте заново!'
             user_data['mode'] = 'VOTING'
-            _markup = markup
         else:
             message = u'Не понятно. Давайте еще раз. Вы точно хотите проголосовать за ' + user_data['band'] + u'?'
             _markup = markup_confirm
-        bot.sendMessage(chat_id=update.message.chat_id,
-                        text=message, reply_markup=_markup)
+        bot.sendMessage(chat_id=update.message.chat_id,text=message, reply_markup=_markup)                
 
     def send_feedback(self, bot, update):
         pass
